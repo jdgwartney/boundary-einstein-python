ec0e0b02e456c8b20ef20eea78f7c054580bccd9
jdgwartney <davidg@boundary.com>
2014-12-24 15:23:09 -0800

    Eliminate the use of curl and tar

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import os
+import shutil
 import sys
 import subprocess
+import tarfile
+import urllib
 
 VENV_VERSION = '1.9.1'
 PYPI_VENV_BASE = 'http://pypi.python.org/packages/source/v/virtualenv'
@@ -20,10 +24,11 @@ tgz_file = dirname + '.tar.gz'
 
 # Fetch virtualenv from PyPI
 venv_url = PYPI_VENV_BASE + '/' + tgz_file
-shellcmd('curl -O {0}'.format(venv_url))
+urllib.urlretrieve(venv_url,tgz_file)
 
 # Untar
-shellcmd('tar xzf {0}'.format(tgz_file))
+tar = tarfile.open(tgz_file,"r:gz")
+tar.extractall()
 
 # Create the initial env
 shellcmd('{0} {1}/virtualenv.py {2}'.format(PYTHON, dirname, INITIAL_ENV))
@@ -32,4 +37,5 @@ shellcmd('{0} {1}/virtualenv.py {2}'.format(PYTHON, dirname, INITIAL_ENV))
 shellcmd('{0}/bin/pip install {1}'.format(INITIAL_ENV, tgz_file))
 
 # Cleanup
-shellcmd('rm -rf {0} {1}'.format(dirname, tgz_file))
+os.remove(tgz_file)
+shutil.rmtree(dirname)
